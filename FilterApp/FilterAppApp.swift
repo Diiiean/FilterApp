b012f5d2e8e6f9fa835a845c766d8d4dbe427541
Diana Nuansengsy <86627602+Diiiean@users.noreply.github.com>
2023-08-14 13:03:54 +0300

    Initial Commit

---
+++ FilterApp/FilterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterAppApp.swift
+//  FilterApp
+//
+//  Created by Диана Нуансенгси on 14.08.23.
+//
+
+import SwiftUI
+
+@main
+struct FilterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
